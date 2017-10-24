4cfd9c63ea59d65f48f36130dcf5729f39106331
meirav Feiler <meiravfeiler@meiravs-MacBook-Pro.local>
2017-10-24 15:06:14 +0900

    auto signed

---
+++ build/Pods.build/Debug-iphonesimulator/Pods-New.build/DerivedSources/Pods_New_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_NewVersionString[];
+ extern const double Pods_NewVersionNumber;
+
+ const unsigned char Pods_NewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_New  PROJECT:Pods-1" "\n";
+ const double Pods_NewVersionNumber __attribute__ ((used)) = (double)1.;
