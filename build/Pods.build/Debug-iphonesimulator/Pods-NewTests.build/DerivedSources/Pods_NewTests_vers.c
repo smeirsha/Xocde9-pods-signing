197b9ef35cd8373233a9708522f79a776d668132
meirav Feiler <meiravfeiler@meiravs-MacBook-Pro.local>
2017-10-24 13:32:20 +0900

    auto sign pods

---
+++ build/Pods.build/Debug-iphonesimulator/Pods-NewTests.build/DerivedSources/Pods_NewTests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_NewTestsVersionString[];
+ extern const double Pods_NewTestsVersionNumber;
+
+ const unsigned char Pods_NewTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_NewTests  PROJECT:Pods-1" "\n";
+ const double Pods_NewTestsVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ build/Pods.build/Debug-iphonesimulator/Pods-NewUITests.build/DerivedSources/Pods_NewUITests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_NewUITestsVersionString[];
+ extern const double Pods_NewUITestsVersionNumber;
+
+ const unsigned char Pods_NewUITestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_NewUITests  PROJECT:Pods-1" "\n";
+ const double Pods_NewUITestsVersionNumber __attribute__ ((used)) = (double)1.;
